80ec58dcd15e37718b4639b369700f0f719608d5
BatoolGasser <batoolgasser5@gmail.com>
2024-02-15 01:01:24 +0200

    "Models and serializers"

---
+++ api/models.py
@@ -0,0 +1,36 @@
+import uuid
+
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+import uuid as uuid_lib
+
+
+class Meal(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+    title = models.CharField(max_length=32)
+    description = models.TextField(max_length=360)
+
+    def __str__(self):
+        return self.title
+
+
+class Rating(models.Model):
+    id = models.UUIDField(
+        default=uuid.uuid4,
+        unique=True,
+        primary_key=True,
+        editable=False
+    )
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    Rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+        unique_together = ('user', 'meal')
+        index_together = ('user', 'meal')
